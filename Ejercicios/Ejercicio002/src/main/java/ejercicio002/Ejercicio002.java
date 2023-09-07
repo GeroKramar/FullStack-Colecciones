9e25cb4f4e6f29304dae1e4532872c9816a61243
GeroKramar <gerokramar@gmail.com>
2023-09-06 22:15:00 -0300

    ej1

---
+++ Ejercicios/Ejercicio002/src/main/java/ejercicio002/Ejercicio002.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package ejercicio002;
+
+/**
+ *
+ * @author usuario
+ */
+public class Ejercicio002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
